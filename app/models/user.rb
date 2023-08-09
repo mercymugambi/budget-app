470764920c3f208af8098ff7096c032b85944a39
Mercy Mugambi <mercymugambi2016@gmail.com>
2023-08-09 21:55:20 +0300

    Add association for user table and other tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :entities, foreign_key: 'author_id'
+  has_many :groups
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
